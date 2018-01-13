27d5cdc12c30c6caf597e888a4a26ab3ce893184
Boss Nam <popman1105@gmail.com>
2018-01-13 12:17:15 +0900

    Flexbox 라이브러리 추가, 메뉴 양옆정렬

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { HomeComponent } from './home/home.component';
 import { JqueryComponent } from './jquery/jquery.component';
-import {AppRoutingModule} from './app-routing.module';
-import {MatIconModule, MatMenuModule, MatToolbar, MatToolbarModule} from '@angular/material';
+import {AppRoutingModule} from "./app-routing.module";
+import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 @NgModule({
@@ -19,10 +21,13 @@ import {MatIconModule, MatMenuModule, MatToolbar, MatToolbarModule} from '@angul
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    FlexLayoutModule,
     MatToolbarModule,
+    MatMenuModule,
     MatIconModule,
-    MatMenuModule
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
